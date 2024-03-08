b44ed54baba2c0654bd1e3610adb514352fced1a
Aish Mishra <aishlunatic001@gmail.com>
2024-03-08 14:34:18 +0530

    keys and it's improtance in react

--- Week6/React Hooks/Keys in React/Keys/src/App.jsx
+++ Week6/React Hooks/Keys in React/Keys/src/App.jsx
@@ -3,16 +3,20 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
+let counter = 4 ; 
 function App() {
   const [todos, setTodos] = useState([{
+    id: 1 ,
     title : "Wake up early",
     description : "6-7 am "
   },
   {
+    id: 2 ,
     title : "Wake up early",
     description : "6-7 am "
   },
   {
+    id: 3 ,
     title : "Wake up early",
     description : "6-7 am "
   }])
@@ -20,7 +24,8 @@ function App() {
   function addTodo(){
     setTodos([...todos, 
        {
-        title: Math.random() ,
+        id: counter++ ,  
+        title: Math.random(),
          description: Math.random() 
         }
       ]);
@@ -29,7 +34,7 @@ function App() {
   return (
     <>
       <button onClick={addTodo}>Add a Todo</button>
-      {todos.map((todo)=> <Todo title = {todo.title} description={todo.description}></Todo>)}
+      {todos.map((todo)=> <Todo key={todo.id} title = {todo.title} description={todo.description}></Todo>)}
     </>
   )
 }
@@ -42,3 +47,15 @@ function Todo({title , description}){
 }
 
 export default App
+
+/*
+
+Note : 
+
+1. When rendering a list of items in React, it's important to provide a unique key to each item in the list. The key prop is a special string attribute you can add to the elements in the list.
+
+2. The key prop helps React identify which items have changed, added, or removed. React uses the key prop to efficiently update and render the list of items.
+
+3. When the key prop changes, React will create a new DOM element for that item. If the key prop remains the same, React will update the existing DOM element instead of recreating it.
+
+*/
