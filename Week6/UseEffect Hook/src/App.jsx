e6b76c8d9e92f03b78815ee21a9b20c9bfaaacd9
Aish Mishra <aishlunatic001@gmail.com>
2024-03-11 22:31:25 +0530

    useEffect but have some bug

--- Week6/UseEffect Hook/src/App.jsx
+++ Week6/UseEffect Hook/src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]) 
 
-  return (
-    <>
-     
-    </>
-  )
+// By default, useEffect runs after the component is rendered and then when its dependencies change. However, it can also run when a component is unmounted if you specify a cleanup function.
+
+// To run a side effect specifically when a component mounts, you can pass an empty dependency array [] to the useEffect hook. In this case, the effect will run only once, after the initial render (component mount).
+
+
+  useEffect(() => {
+    fetch("https://sum-server.100xdevs.com/todos")
+      .then(async function(res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+  }, [])
+
+  return <div>
+      {todos.map((todo)=><Todo key={todo.id} title={todo.title} description={todo.description}></Todo>)}
+    </div>
+  
 }
 
-export default App
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+    </div>
+  );
+}
+
+export default App;
