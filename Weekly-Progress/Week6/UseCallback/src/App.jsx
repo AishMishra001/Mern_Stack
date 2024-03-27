c4db7dd9d006ab5522a03a01811d9b383528b088
Aish Mishra <aishlunatic001@gmail.com>
2024-03-27 15:25:47 +0530

    UseCallback hook

---
+++ Weekly-Progress/Week6/UseCallback/src/App.jsx
@@ -0,0 +1,30 @@
+import { memo,useCallback,useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+ const wtf = useCallback(()=>{
+  console.log('what the fuck!!') ; 
+ },[])
+  return <div>
+      <ButtonComponent inputFunction={wtf}></ButtonComponent>
+      <button onClick={
+       ()=>{
+        setCount(count+1)
+       } 
+      }>counter: {count}</button>
+    </div>
+  
+}
+
+const ButtonComponent = memo(({inputFunction})=>{
+ console.log("Button Rendered")
+ return <div>
+
+   <button>Click me</button>
+
+ </div>
+})
+
+export default App
