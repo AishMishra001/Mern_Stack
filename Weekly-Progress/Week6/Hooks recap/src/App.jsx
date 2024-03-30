40f1a82ac3bf35d203f83452cc49350198f8a5a8
Aish Mishra <aishlunatic001@gmail.com>
2024-03-30 14:15:01 +0530

    useCallback recap

--- Weekly-Progress/Week6/Hooks recap/src/App.jsx
+++ Weekly-Progress/Week6/Hooks recap/src/App.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react'
 
@@ -31,19 +31,27 @@ function App() {
       returns : 100 
      })
   },[5000]);
-})
+},[])
 
-const cryptoReturns = useMemo(()=>{
-   return exchangeData1.returns  +  exchangeData2.returns  
-},[exchangeData1,exchangeData2])
+// useCallback here won't rerender the calculateCryptoReturns are the props exchangeData1 and exchangeData2 is not changed 
+
+const calculateCryptoReturns = useCallback(function(){  
+  return exchangeData1.returns  +  exchangeData2.returns  
+  },[exchangeData1,exchangeData2])
   
-const creditValue = (cryptoReturns + bankData.returns)* 0.3 ; 
 
-  return (
-    <div>
-       Your credit value is {creditValue} 
-    </div>
-  )
+
+return <div>
+   <CryptoGainCalculator calculateCryptoReturns={calculateCryptoReturns}/>
+</div>
+
 }
+
+const CryptoGainCalculator= memo(function({calculateCryptoReturns}){
+  console.log("crypto child re-render")
+  return <div>
+    your crypto returns are {calculateCryptoReturns()} 
+  </div>
+})
 
 export default App
