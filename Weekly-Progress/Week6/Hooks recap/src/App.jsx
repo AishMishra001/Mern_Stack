df27d61710d9ca2e2550d95727795efc2af29cef
Aish Mishra <aishlunatic001@gmail.com>
2024-03-31 22:58:40 +0530

    Use Ref and useMemo assignment

--- Weekly-Progress/Week6/Hooks recap/src/App.jsx
+++ Weekly-Progress/Week6/Hooks recap/src/App.jsx
@@ -1,57 +1,21 @@
-import { memo, useCallback, useMemo } from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
-
+const [incomeTax , setIncomeTax] = useState(20000); // 
 function App() {
-  const [ exchangeData1 , setExchangeData1] = useState({})
-  const [ exchangeData2 , setExchangeData2] = useState({})
-  const [ bankData , setBankData ] = useState({})
+  const divRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(divRef.current)
+      divRef.current.innerHTML = 10 
+    }, 5000);
+  }, [])
 
 
-  useEffect(()=>{
-      
-        setExchangeData1(
-         { returns : 100} 
-        )
-      
-  },[])
-
-  useEffect(()=>{
-     
-        setExchangeData2(
-         { returns : 100} 
-        )
-    
-  },[])
-
-  useEffect(()=>{
-   setInterval(() => {
-     setBankData({
-      returns : 100 
-     })
-  },[5000]);
-},[])
-
-// useCallback here won't rerender the calculateCryptoReturns are the props exchangeData1 and exchangeData2 is not changed 
-
-const calculateCryptoReturns = useCallback(function(){  
-  return exchangeData1.returns  +  exchangeData2.returns  
-  },[exchangeData1,exchangeData2])
-  
-
-
-return <div>
-   <CryptoGainCalculator calculateCryptoReturns={calculateCryptoReturns}/>
-</div>
-
-}
-
-const CryptoGainCalculator= memo(function({calculateCryptoReturns}){
-  console.log("crypto child re-render")
   return <div>
-    your crypto returns are {calculateCryptoReturns()} 
-  </div>
-})
+        hi there, your income tax returns are <div ref={divRef}>{incomeTax}</div>
+    </div>
+  
+}
 
 export default App
