f4a27cfa394c8ac33bca42e3d8fc61d0ca5b3ff5
Aish Mishra <aishlunatic001@gmail.com>
2024-04-01 22:44:48 +0530

    revising week3

--- Weekly-Progress/Week3/Authentication/jwt.js
+++ Weekly-Progress/Week3/Authentication/jwt.js
@@ -19,4 +19,9 @@ console.log(token);
 // Verifying : 
 
 const token1 = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWlzaCBNaXNocmEiLCJhY2NvdW50bnVtYmVyIjoxMjMxMjMxMjMsImlhdCI6MTcwNjE5NDA3Nn0.wh_voOHzOBRTUfnCdf_Z4GNV23Nww3yy3jy9z4I2BCI","secret");
-console.log(token);
+console.log(token1);
+
+
+// decoding : There's no need for jwt.encode() in this context because jwt.sign() already performs both encoding and signing of the JWT token . 
+
+// But say you have the jwt token and you want to see users info that is possible if you go to the website "jwt.io" . Here if you put the jwt you will get the info .
