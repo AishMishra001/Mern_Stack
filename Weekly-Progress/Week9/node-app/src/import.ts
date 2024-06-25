7fb58bf3fb5b97c2c772b3d431defdc53cd3ee7c
Aish Mishra <aishlunatic001@gmail.com>
2024-06-26 03:26:14 +0530

    import export

---
+++ Weekly-Progress/Week9/node-app/src/import.ts
@@ -0,0 +1,12 @@
+// previous way of import was : 
+
+// const express = require("express")
+
+// new ES6 way :
+
+import express from "express" ; 
+import b, { a } from "./export";
+
+
+const app = express() ; 
+
