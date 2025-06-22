1f95919a9f6deca65c84e5432d133273daffb7d2
AishMishra001 <aishlunatic001@gmail.com>
2025-06-22 23:13:41 +0530

    server side render implemention tested

--- Weekly-Progress/Week18/Readme.md
+++ Weekly-Progress/Week18/Readme.md
@@ -1,3 +1,5 @@
 # Nextjs
 
-command to create next app : npx create-next-app@latest
+command to create next app : npx create-next-app@latest 
+
+## Slides : https://projects.100xdevs.com/tracks/nextjs-1/next-1
---
+++ Weekly-Progress/Week18/nextapp/app/blogs/page.tsx
@@ -0,0 +1,32 @@
+// Implementing server-side rendering 
+
+import axios from 'axios' ; 
+
+async function getBlogs(){
+  const response = await axios.get("https://jsonplaceholder.typicode.com/todos/") 
+  return response.data ; 
+}
+
+export default async function Blogs(){
+
+  const blogs = await getBlogs() ; 
+  return <div>
+    {blogs.map((blog : ITodo)=><Todo title={blog.title} completed={blog.completed}/>)}
+  </div>
+}
+
+interface ITodo {
+  title : string , 
+  completed : boolean 
+}
+
+function Todo({title , completed} : ITodo){
+  return <div className='flex space-x-2'>
+    <div className='border-black-200 border display: inline-block'>
+      {title}
+    </div>
+    <div className='font-bold'>
+     {completed? "done" : "not done"}
+    </div>
+  </div>
+}
